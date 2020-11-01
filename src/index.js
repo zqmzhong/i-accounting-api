10daf792ba7d6661eac75a83af4c4e3d67651cd3
Frank Bell <frank.zhong@morningstar.com>
2020-11-01 22:06:18 +0800

    Enable CORS

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 const Koa = require('koa');
 const serverless = require('serverless-http');
+const cors = require('@koa/cors');
 const router = require('./router');
 
 const app = new Koa();
+app.use(cors());
 app.use(router.routes()).use(router.allowedMethods());
 
 module.exports.app = serverless(app);
