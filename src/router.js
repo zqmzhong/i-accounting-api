dbc9ab42f2185dd5f10b37de59eed6eee2af2fc1
Francis Bell <zqmzhong@gmail.com>
2020-11-01 16:12:16 +0800

    Integrate DynamoDB to save bills

--- src/router.js
+++ src/router.js
@@ -1,7 +1,10 @@
 const router = require('koa-router')();
 const healthService = require('./service/health-check');
+const billService = require('./service/bill');
 
 router.get(`/index`, healthService.index);
 router.get(`/health`, healthService.health);
+
+router.get('/bills', billService.getAllBills);
 
 module.exports = router;
